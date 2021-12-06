54ee4eab98e1ed77dc64693fb08755daa981ecc0
owlloong <owlloong@foxmail.com>
2021-12-06 14:51:48 +0800

    Added by OwlLoong:First Commit，项目初始化

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Time: 2021-12-6 14:41:53<br/>
+Description: Myself learning research<br/>
+Holder: OwlLoong Yuan<br/>
+E-mail: owlloong@outlook.com
+
---
+++ src/test/java/com/owl/myshop/MyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.myshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
