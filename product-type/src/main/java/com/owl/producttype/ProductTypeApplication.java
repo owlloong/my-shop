859ac12231679a8c384faf75c4853ee59f9e292e
owlloong <owlloong@foxmail.com>
2022-01-06 15:35:01 +0800

    Added by OwlLoong:商品类别管理初步实现

---
+++ product-type/src/main/java/com/owl/producttype/ProductTypeApplication.java
@@ -0,0 +1,13 @@
+package com.owl.producttype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductTypeApplication.class, args);
+	}
+
+}
