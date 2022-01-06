b6443ef3c565bcb2b80a7dd7b32f43ff95a1968d
owlloong <owlloong@foxmail.com>
2022-01-06 15:34:38 +0800

    Added by OwlLoong:用户管理初步实现

---
+++ product-type/src/test/java/com/owl/producttype/ProductTypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.producttype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
